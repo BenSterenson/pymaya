a2e72d95d46fbf08f672c88fc8c47952e0e1d132
bensterenson <ben@cybersixgill.com>
2020-08-19 09:58:49 +0300

    fix type annotation

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="pymaya",
-    version="0.1.1",
+    version="0.1.2",
     author="Ben Sterenson",
     author_email="bensterenson@gmail.com",
     description="Python client to interact with maya.tase.co.il",
